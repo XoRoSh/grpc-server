22bae8d79dcbd697f44779fb56ec20ca508b045c
Lev Ganzha <levganzha@Levs-Mac-mini.local>
2025-01-18 19:39:11 +0000

    initial

---
+++ go.mod
@@ -0,0 +1,20 @@
+module grpc
+
+go 1.23.4
+
+require (
+	google.golang.org/grpc v1.69.4
+	google.golang.org/protobuf v1.36.3
+	gorm.io/driver/sqlite v1.5.7
+	gorm.io/gorm v1.25.12
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241015192408-796eee8c2d53 // indirect
+)
---
+++ grpc_server.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/fieldmaskpb"
+
+	pb "graph/data"
+)
+
+// Database model
+type Data struct {
+	ID          string `gorm:"primaryKey"`
+	Name        string
+	Description string
+}
+
+// gRPC server
+type server struct {
+	pb.UnimplementedDataServiceServer
+	db *gorm.DB
+}
+
+func createFieldMask(paths []string) *fieldmaskpb.FieldMask {
+	return &fieldmaskpb.FieldMask{
+		Paths: paths,
+	}
+}
+
+func (s *server) GetData(ctx context.Context, req *pb.DataRequest) (*pb.DataResponse, error) {
+	var data Data
+	if err := s.db.First(&data, "id = ?", req.Id).Error; err != nil {
+		return nil, err
+	}
+
+	// Handle FieldMask
+	response := &pb.DataResponse{}
+	for _, path := range req.FieldMask.GetPaths() {
+		switch path {
+		case "id":
+			response.Id = data.ID
+		case "name":
+			response.Name = data.Name
+		case "description":
+			response.Description = data.Description
+		}
+	}
+
+	return response, nil
+}
+
+func main() {
+	// Initialize database
+	db, err := gorm.Open(sqlite.Open("data.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	db.AutoMigrate(&Data{}) // Migrate the schema
+
+	// Seed data
+	db.Create(&Data{ID: "1", Name: "Example", Description: "This is a sample data entry"})
+
+	// Start gRPC server
+	listener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterDataServiceServer(grpcServer, &server{db: db})
+
+	log.Println("gRPC server is running on port 50051")
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
